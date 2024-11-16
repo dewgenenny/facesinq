c230e13055b758ad128e6df1dddf7a43bcfbe2cc
dewgenenny <tom@penberth.com>
2024-11-16 16:09:29 +0100

    fix DB creation

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///instance/facesinq.db"
+DATABASE_URL = "sqlite:///facesinq.db"
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
