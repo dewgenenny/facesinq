700985a81870a63421201fa5d8f047ff01e52996
dewgenenny <tom@penberth.com>
2024-11-16 11:52:53 +0100

    debugging signup

--- utils.py
+++ utils.py
@@ -1,16 +1,13 @@
-# utils.py
-import os
+# utils.py (refactor fetch_and_store_users)
+from models import db, User
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-import sqlite3
+import os
 
-# utils.py
 def fetch_and_store_users():
     SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
     client = WebClient(token=SLACK_BOT_TOKEN)
 
-    conn = sqlite3.connect('facesinq.db')
-    c = conn.cursor()
     try:
         response = client.users_list()
         for member in response['members']:
@@ -18,8 +15,14 @@ def fetch_and_store_users():
                 user_id = member['id']
                 name = member['real_name']
                 image = member['profile'].get('image_192', '')
-                c.execute('INSERT OR IGNORE INTO users (id, name, image) VALUES (?, ?, ?)', (user_id, name, image))
+
+                # Check if user already exists
+                existing_user = User.query.get(user_id)
+                if not existing_user:
+                    # Create a new user
+                    new_user = User(id=user_id, name=name, image=image)
+                    db.session.add(new_user)
+
+        db.session.commit()
     except SlackApiError as e:
         print(f"Error fetching users: {e.response['error']}")
-    conn.commit()
-    conn.close()
